dc6f7d85d65fa620e14ad66caecb40fe21d7c5cd
SomeBody16 <filip.nowakowski2000@outlook.com>
2019-05-19 19:35:10 +0200

    Updates

---
+++ precache-manifest.bb43ecad9c069bfec83a88adc452b638.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e1209e9f0829f9e0fb067a93c0750b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5540eeb85a61350dd5e4",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "21e21653eeed6e9319c4",
+    "url": "/static/js/2.c7dd1cee.chunk.js"
+  },
+  {
+    "revision": "5540eeb85a61350dd5e4",
+    "url": "/static/js/main.4a7d524f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
